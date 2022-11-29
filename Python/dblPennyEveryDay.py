2a1a967dd21b114841a4b5a4eaec510048305fc7
njs33487 <novemberjulietsierra334@gmail.com>
2022-11-28 22:00:21 -0500

    organized file structure update

---
+++ Python/dblPennyEveryDay.py
@@ -0,0 +1,6 @@
+def dblPenny_perDay(days):  
+    penny = 0.01
+    print((2**days)*penny)
+
+days = int(input(" How many days: "))
+dblPenny_perDay(days)
---
+++ Python/pythonAdv3.py
@@ -0,0 +1,5 @@
+file_path = "C:\Users\njs33\OneDrive\Documents\Desktop\gitRepo\Python\Machine_Learning_AI (1).txt"
+
+# code to open file:
+our_text_file = open(file_path,'r') 
+
